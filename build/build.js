88a30d7f95452fc37166150fb58b41e33cc4e745
chemzqm <chemzqm@gmail.com>
2014-06-28 02:21:42 +0800

    Update documentation

--- build/build.js
+++ build/build.js
@@ -166,89 +166,43 @@ function parse(html) {
 
 });
 
-require.register("component~raf@1.1.3", function (exports, module) {
+require.register("component~debounce@0.0.3", function (exports, module) {
 /**
- * Expose `requestAnimationFrame()`.
+ * Debounces a function by the given threshold.
+ *
+ * @see http://unscriptable.com/2009/03/20/debouncing-javascript-methods/
+ * @param {Function} function to wrap
+ * @param {Number} timeout in ms (`100`)
+ * @param {Boolean} whether to execute at the beginning (`false`)
+ * @api public
  */
 
-exports = module.exports = window.requestAnimationFrame
-  || window.webkitRequestAnimationFrame
-  || window.mozRequestAnimationFrame
-  || window.oRequestAnimationFrame
-  || window.msRequestAnimationFrame
-  || fallback;
+module.exports = function debounce(func, threshold, execAsap){
+  var timeout;
 
-/**
- * Fallback implementation.
- */
+  return function debounced(){
+    var obj = this, args = arguments;
 
-var prev = new Date().getTime();
-function fallback(fn) {
-  var curr = new Date().getTime();
-  var ms = Math.max(0, 16 - (curr - prev));
-  var req = setTimeout(fn, ms);
-  prev = curr;
-  return req;
-}
+    function delayed () {
+      if (!execAsap) {
+        func.apply(obj, args);
+      }
+      timeout = null;
+    }
 
-/**
- * Cancel.
- */
+    if (timeout) {
+      clearTimeout(timeout);
+    } else if (execAsap) {
+      func.apply(obj, args);
+    }
 
-var cancel = window.cancelAnimationFrame
-  || window.webkitCancelAnimationFrame
-  || window.mozCancelAnimationFrame
-  || window.oCancelAnimationFrame
-  || window.msCancelAnimationFrame
-  || window.clearTimeout;
-
-exports.cancel = function(id){
-  cancel.call(window, id);
+    timeout = setTimeout(delayed, threshold || 100);
+  };
 };
 
 });
 
-require.register("component~per-frame@2.0.1", function (exports, module) {
-/**
- * Module Dependencies.
- */
-
-var raf = require("component~raf@1.1.3");
-
-/**
- * Export `throttle`.
- */
-
-module.exports = throttle;
-
-/**
- * Executes a function at most once per animation frame. Kind of like
- * throttle, but it throttles at ~60Hz.
- *
- * @param {Function} fn - the Function to throttle once per animation frame
- * @return {Function}
- * @public
- */
-
-function throttle(fn) {
-  var queued = false;
-
-  return function queue() {
-    if (queued) return;
-    queued = true;
-    var ctx = this;
-    var args = arguments;
-
-    raf(function() {
-      queued = false;
-      return fn.apply(ctx, args);
-    });
-  };
-}
-
-});
-
-require.register("component~event@0.1.3", function (exports, module) {
+require.register("component~event@0.1.4", function (exports, module) {
 var bind = window.addEventListener ? 'addEventListener' : 'attachEvent',
     unbind = window.removeEventListener ? 'removeEventListener' : 'detachEvent',
     prefix = bind !== 'addEventListener' ? 'on' : '';
@@ -288,41 +242,37 @@ exports.unbind = function(el, type, fn, capture){
 
 require.register("more", function (exports, module) {
 var styles = require("chemzqm~computed-style@0.1.1");
-var events = require("component~event@0.1.3");
+var events = require("component~event@0.1.4");
 var domify = require("component~domify@1.2.2");
-var throttle = require("component~per-frame@2.0.1");
+var debounce = require("component~debounce@0.0.3");
 var template = require("more/template.html");
 
-function More(el, fn) {
-  if (!(this instanceof More)) return new More(el, fn);
+function More(el, fn, scrollable) {
+  if (!(this instanceof More)) return new More(el, fn, scrollable);
   this.el = el;
   this.callback = fn;
-  while (el) {
-    if (styles(el).overflowY === 'scroll') break;
-    el = el.parentNode;
-    if (el === document.documentElement) el = null;
-  }
   this.div = domify(template);
   insertAfter(this.el, this.div);
-  var scrollable = el ? el : window;
+  scrollable = scrollable || el.parentNode;
   this.onscroll();
-  var onscroll = throttle(this.onscroll.bind(this));
-  events.bind(scrollable, 'scroll', onscroll);
+  var self = this;
+  scrollable.addEventListener('scroll', debounce(function () {
+    self.onscroll();
+  }), false);
 }
 
 More.prototype.onscroll = function () {
   if (this.loading || this._disabled) return;
   if (!check(this.el)) return;
-  var self = this;
   this.div.style.display = 'block';
-  this.callback.call(this, cb);
   var h = styles(this.el).height;
   this.loading = true;
-  function cb(disable) {
+  var self = this;
+  this.callback(function (disable) {
     if (disable) self.disable();
     self.loading = false;
     self.div.style.display = 'none';
-  }
+  });
 }
 
 More.prototype.disable = function () {

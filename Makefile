9cdd476fed763fc18fdb101fc666b22991d27b6c
chemzqm <chemzqm@gmail.com>
2015-11-13 00:20:14 +0800

    add test

--- Makefile
+++ Makefile
@@ -1,4 +1,3 @@
-
 dev:
 	@webpack example/index.js example/bundle.js --module-bind html --module-bind "css=style!css" --module-bind "png=url-loader?mimetype=image/png"
 
@@ -6,4 +5,16 @@ doc:
 	@webpack example/index.js example/bundle.js --module-bind html --module-bind "css=style!css" --module-bind "png=url-loader?mimetype=image/png"
 	@ghp-import example -n -p
 
-.PHONY: clean doc
+test:
+	@open http://localhost:8080/bundle
+	@webpack-dev-server 'mocha!./test/test.js' --inline --hot --module-bind html
+
+test-karma:
+	@node_modules/.bin/karma start --single-run
+
+test-coveralls:
+	@echo TRAVIS_JOB_ID $(TRAVIS_JOB_ID)
+	@node_modules/.bin/karma start --single-run && \
+		cat ./coverage/lcov/lcov.info | ./node_modules/coveralls/bin/coveralls.js
+
+.PHONY: clean doc test
---
+++ test/test.js
@@ -0,0 +1,230 @@
+/*global describe, it, beforeEach, afterEach*/
+var assert = require('assert')
+var More = require('..')
+
+var ul
+var scrollable
+
+function assign(to, from) {
+  Object.keys(from).forEach(function (k) {
+    to[k] = from[k]
+  })
+  return to
+}
+
+beforeEach(function () {
+  scrollable = document.createElement('div')
+  assign(scrollable.style, {
+    position: 'fixed',
+    top: '0px',
+    left: '0px',
+    width: '100px',
+    height: '300px',
+    padding: '0px',
+    margin: '0px',
+    overflow: 'hidden'
+  })
+  ul = document.createElement('ul')
+  for (var i = 7; i >= 0; i--) {
+    var li = document.createElement('li')
+    li.style.height = '50px'
+    li.textContent = i
+    ul.appendChild(li)
+  }
+  scrollable.appendChild(ul)
+  document.body.appendChild(scrollable)
+})
+
+afterEach(function () {
+  document.body.removeChild(scrollable)
+})
+
+describe('More()', function() {
+  it('should init with new', function () {
+    var m = new More(ul, function () {})
+    assert.equal(m.el, ul)
+    m.remove()
+  })
+
+  it('should init without new', function () {
+    var m = More(ul, function () {}, scrollable)
+    assert.equal(m.el, ul)
+    m.remove()
+  })
+
+  it('should insert div after list', function () {
+    scrollable.appendChild(document.createElement('div'))
+    var m = More(ul, function () {}, scrollable)
+    assert.equal(m.el, ul)
+    var next = ul.nextElementSibling
+    assert.equal(next, m.div)
+    m.remove()
+  })
+})
+
+describe('onscroll', function () {
+  it('should load when scroll to bottom', function (done) {
+    var fired
+    var m = More(ul, function () {
+      fired = true
+    }, scrollable)
+    scrollable.scrollTop = scrollable.scrollHeight
+    setTimeout(function () {
+      assert.equal(fired, true)
+      assert.equal(m.div.style.display, 'block')
+      m.remove()
+      done()
+    }, 100)
+  })
+
+  it('should not load when not scroll to bottom', function (done) {
+    var fired
+    var m = More(ul, function (cb) {
+      fired = true
+      cb()
+    }, scrollable)
+    scrollable.scrollTop = 5
+    setTimeout(function () {
+      assert.notEqual(fired, true)
+      //assert.equal(m.div.style.display, 'none')
+      m.remove()
+      done()
+    }, 100)
+  })
+
+  it('should hide the div after load', function (done) {
+    var m = More(ul, function (cb) {
+      cb()
+    }, scrollable)
+    scrollable.scrollTop = scrollable.scrollHeight
+    setTimeout(function () {
+      assert.equal(m.div.style.display, 'none')
+      done()
+    }, 100)
+  })
+
+  it('should be promise awared', function (done) {
+    var m = More(ul, function () {
+      return new Promise(function (resolve) {
+        resolve()
+      })
+    }, scrollable)
+    scrollable.scrollTop = scrollable.scrollHeight
+    setTimeout(function () {
+      assert.equal(m.div.style.display, 'none')
+      done()
+    }, 100)
+  })
+
+  it('should hide div on rejected', function (done) {
+    var m = More(ul, function () {
+      return new Promise(function (resolve, reject) { // eslint-disable-line
+        reject(new Error('request error'))
+      })
+    }, scrollable)
+    scrollable.scrollTop = scrollable.scrollHeight
+    setTimeout(function () {
+      assert.equal(m.div.style.display, 'none')
+      done()
+    }, 100)
+  })
+})
+
+describe('scroll window', function () {
+  var list = document.createElement('ul')
+  document.body.appendChild(list)
+  beforeEach(function () {
+    var vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+    list.style.height = (vh + 50) + 'px'
+    window.scrollTo(0, 0)
+  })
+
+  afterEach(function () {
+    window.scrollTo(0, 0)
+  })
+
+  it('should not load when not scroll to bottom', function (done) {
+    var fired
+    var m = More(list, function () {
+      fired = true
+    }, window)
+    window.scrollTo(0, 10)
+    setTimeout(function () {
+      assert.notEqual(fired, true)
+      m.remove()
+      done()
+    }, 100)
+  })
+
+  it('should load when scroll to bottom', function (done) {
+    var fired
+    var m = More(list, function () {
+      fired = true
+    }, window)
+    var sh = document.documentElement.scrollHeight
+    var vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+    window.scrollTo(0, sh - vh)
+    setTimeout(function () {
+      assert.equal(fired, true)
+      m.remove()
+      done()
+    }, 100)
+  })
+})
+
+describe('.disable()', function() {
+  it('should disable loading', function (done) {
+    var fired
+    var m = More(ul, function (cb) {
+      fired = true
+      cb()
+    }, scrollable)
+    m.disable()
+    scrollable.scrollTop = scrollable.scrollHeight
+    setTimeout(function () {
+      assert.equal(m.div.style.display, 'none')
+      assert.notEqual(fired, true)
+      done()
+    }, 100)
+  })
+})
+
+describe('.text()', function() {
+  it('should change text', function (done) {
+    var m = More(ul, function(cb){
+      cb()
+    }, scrollable)
+    m.text('loading')
+    assert(m.div.textContent.indexOf('loading') !== -1)
+    done()
+  })
+})
+
+describe('.remove()', function() {
+  it('should unbind event listeners', function (done) {
+    var fired
+    var m = More(ul, function(cb){
+      fired = true
+      cb()
+    }, scrollable)
+    m.remove()
+    scrollable.scrollTop = scrollable.scrollHeight
+    setTimeout(function () {
+      assert.notEqual(fired, true)
+      done()
+    }, 100)
+  })
+})
+
+describe('.load()', function() {
+  it('should force loading data', function (done) {
+    var fired
+    var m = More(ul, function (cb) {
+      fired = true
+      cb()
+    })
+    m.load()
+    assert.equal(fired, true)
+    done()
+  })
+})
